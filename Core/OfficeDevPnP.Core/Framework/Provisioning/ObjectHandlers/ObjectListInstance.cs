c18da168d55d0cd7cda45f73fb2a4c94113716d7
Henrik Mathiassen <hm@workpoint.dk>
2017-05-30 10:29:50 +0200

    Removed code that makes fields not used in a contenttypes extracted

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectListInstance.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectListInstance.cs
@@ -1787,13 +1787,13 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
                 if (siteColumn != null)
                 {
                     var addField = true;
-                    if (siteList.ContentTypesEnabled && contentTypeFields.FirstOrDefault(c => c.Id == field.Id) == null)
-                    {
-                        if (contentTypeFields.FirstOrDefault(c => c.Id == field.Id) == null)
-                        {
-                            addField = false;
-                        }
-                    }
+                    //if (siteList.ContentTypesEnabled && contentTypeFields.FirstOrDefault(c => c.Id == field.Id) == null)
+                    //{
+                    //    if (contentTypeFields.FirstOrDefault(c => c.Id == field.Id) == null)
+                    //    {
+                    //        addField = false;
+                    //    }
+                    //}
 
                     if (siteColumn.DefaultValue != field.DefaultValue)
                     {
