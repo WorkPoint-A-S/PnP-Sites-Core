018af377c536f5194a065e5b2bebd9afb77ccc31
Michael Schau <schau@outlook.dk>
2019-02-14 09:03:19 +0100

    Fixed merge conflict

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/Extensions/UserResourceExtensions.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/Extensions/UserResourceExtensions.cs
@@ -17,12 +17,13 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.Extensions
 #if !SP2013
     internal static class UserResourceExtensions
     {
+        private static List<Tuple<string, int, string>> ResourceTokens = new List<Tuple<string, int, string>>();
 
         public static ProvisioningTemplate SaveResourceValues(ProvisioningTemplate template, ProvisioningTemplateCreationInformation creationInfo)
         {
             var tempFolder = System.IO.Path.GetTempPath();
 
-            var languages = new List<int>(creationInfo.ResourceTokens.Keys.Select(t => t.Item2).Distinct());
+            var languages = new List<int>(ResourceTokens.Select(t => t.Item2).Distinct());
             foreach (int language in languages)
             {
                 var culture = new CultureInfo(language);
@@ -40,9 +41,11 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.Extensions
                         foreach (DictionaryEntry entry in resxReader)
                         {
                             // find if token is already there
-                            if (!creationInfo.ResourceTokens.ContainsKey(new Tuple<string, int>(entry.Key.ToString(), language)))
-                                creationInfo.ResourceTokens.Add(new Tuple<string, int>(entry.Key.ToString(), language), entry.Value as string);
-                            
+                            var existingToken = ResourceTokens.FirstOrDefault(t => t.Item1 == entry.Key.ToString() && t.Item2 == language);
+                            if (existingToken == null)
+                            {
+                                ResourceTokens.Add(new Tuple<string, int, string>(entry.Key.ToString(), language, entry.Value as string));
+                            }
                         }
                     }
                 }
@@ -54,10 +57,10 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.Extensions
                 using (ResourceWriter resx = new ResourceWriter(resourceFileName))
 #endif
                 {
-                    foreach (var token in creationInfo.ResourceTokens.Where(t => t.Key.Item2 == language))
+                    foreach (var token in ResourceTokens.Where(t => t.Item2 == language))
                     {
 
-                        resx.AddResource(token.Key.Item1, token.Value);
+                        resx.AddResource(token.Item1, token.Item3);
                     }
                 }
 
@@ -115,8 +118,7 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.Extensions
                 if (!string.IsNullOrEmpty(value.Value))
                 {
                     returnValue = true;
-                    if (!creationInfo.ResourceTokens.ContainsKey(new Tuple<string, int>(token, language.LCID)))
-                        creationInfo.ResourceTokens.Add(new Tuple<string, int>(token, language.LCID), value.Value);
+                    ResourceTokens.Add(new Tuple<string, int, string>(token, language.LCID, value.Value));
                 }
             }
 
@@ -139,7 +141,7 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.Extensions
         public static bool PersistResourceValue(List siteList, Guid viewId, string token, ProvisioningTemplate template, ProvisioningTemplateCreationInformation creationInfo)
         {
             bool returnValue = false;
-            var clientContext= siteList.Context;
+            var clientContext = siteList.Context;
 
             foreach (var language in template.SupportedUILanguages)
             {
@@ -150,7 +152,7 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.Extensions
                 clientContext.PendingRequest.RequestExecutor.WebRequest.Headers["Accept-Language"] = new CultureInfo(language.LCID).Name;
                 clientContext.ExecuteQueryRetry();
 
-                if(!string.IsNullOrWhiteSpace(currentView.Title))
+                if (!string.IsNullOrWhiteSpace(currentView.Title))
                 {
                     returnValue = true;
                     ResourceTokens.Add(new Tuple<string, int, string>(token, language.LCID, currentView.Title));
